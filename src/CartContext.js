442c2964ae213f07b881a1eab05ada6f59a3427c
avalle <alicio.valle242@gmail.com>
2025-01-09 21:49:43 -0500

    added state for CartContext

--- src/CartContext.js
+++ src/CartContext.js
@@ -2,5 +2,30 @@ import { createContext } from "react";
 import { productArray } from "./ProductsStore";
 
 const CartContext = createContext({
-
+    items: [],
+    getProductQuantity: () => { },
+    addOneToCart: () => { },
+    removeOneFromCart: () => { },
+    deleteFromCart: () => { },
+    getTotalCost: () => { }
 });
+
+export function CartProvider({ children }) {
+    const [cartProducts, setCartProducts] = useState([]);
+
+
+    const contextValue = {
+        items: [],
+        getProductQuantity,
+        addOneToCart,
+        removeOneFromCart,
+        deleteFromCart,
+        getTotalCost
+    }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    )
+}
